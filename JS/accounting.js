0fec14e97e787aab366238b1576a6a7aa6f79ae4
WalidAlrefai <w.alrefai.3@gmail.com>
2022-01-26 18:20:37 +0200

    last edit

---
+++ JS/accounting.js
@@ -0,0 +1,38 @@
+// 'use strict';
+// let depTable =document.getElementById("depTable");
+// let table =document.getElementById("table");
+
+// let data =localStorage.getItem("Employees");
+//     let parsedData =JSON.parse(data);
+
+
+//     console.log(parsedData[0].Department);
+//     // let table = document.createElement("table");
+    
+//     let thead = document.createElement("thead");
+//     let tbody = document.createElement("tbody");
+//     let tfoot = document.createElement("tfoot");
+//     let th = document.createElement("th");
+//     let th1 = document.createElement("th");
+//     let th2 = document.createElement("th");
+//     let th3 = document.createElement("th");
+//     let th4 = document.createElement("th");
+//     // depTable.appendChild(table);
+//     table.appendChild(thead);
+//     table.appendChild(tfoot);
+//     table.appendChild(tbody);
+//     thead.appendChild(th);
+//     thead.appendChild(th1);
+//     thead.appendChild(th2);
+//     thead.appendChild(th3);
+//     thead.appendChild(th4);
+//     th.textContent = "Department";
+//     th1.textContent = "Number of  employees";
+//     th2.textContent = "Total Salary";
+//     th3.textContent ="Average" ;
+//     th4.textContent = "Total";
+    
+
+
+
+
